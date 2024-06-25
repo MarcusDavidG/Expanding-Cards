bac3276c00ac329cb6fb3a96340ba2b5fe258e8b
MARCUS DAVID <davidmarcus827@gmail.com>
2024-06-25 22:23:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      cards.forEach((c) => c.classList.remove("expanded"));
+      card.classList.add("expanded");
+    });
+  });
+});
